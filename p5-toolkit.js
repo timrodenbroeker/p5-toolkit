acf91e95f94bd67bbabc948805806c16f144a5b4
Tim Rodenbröker <post@timrodenbroeker.de>
2024-01-04 22:36:07 +0100

    l

--- p5-toolkit.js
+++ p5-toolkit.js
@@ -8,12 +8,21 @@ function draw() {
     background(255,0,0);
     noStroke();
     
-    let diameter = document.getElementById("diameter").value * width / 100;
-    let gray = parseFloat(document.getElementById("gray").value);
-
+    
     noStroke();
-    fill(gray);
 
+    let gray = parseFloat(document.getElementById("gray").value);
+    fill(gray);
+    
+    let diameter = document.getElementById("diameter").value * width / 100;
     circle(width/2,height/2,diameter);
 
+    let headline = document.getElementById("headline");
+    textSize(100);
+    text(headline.value,20,20,width-40,height-40);
+
 }
+
+document.getElementById("save").addEventListener("click", function() {
+    save("a.png");
+}); 
